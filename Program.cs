540f828b6f2a04e47b94f282fb933e8a15dcf163
hazinX7 <adm.owner@mail.ru>
2025-01-17 21:27:58 +0700

    Domain_V3

--- Program.cs
+++ Program.cs
@@ -21,15 +21,6 @@ namespace hazinDNS_v2
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.WebHost.UseUrls("http://+:8080");
-            builder.WebHost.ConfigureKestrel(serverOptions =>
-            {
-                serverOptions.ListenAnyIP(8080, options => 
-                {
-                    options.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1;
-                });
-            });
-
             builder.Services.AddDistributedMemoryCache();
             builder.Services.AddSession(options =>
             {
